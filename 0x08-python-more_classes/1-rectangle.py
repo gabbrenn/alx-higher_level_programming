45649480ed342114ae43c21db775afedccf69a6a
gabbrenn <gabvladimirbrenn@gmail.com>
2023-11-27 14:18:40 -0800

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self._width = 0
+        self._height = 0
+        self.width = width
+        self.height = height
+
+
+    def width(self):
+        return self._width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = value
+
+
+    def height(self):
+        return self._height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = value
