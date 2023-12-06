b6fab62e1197d231da97128ef9c3a08f4a1fb2a9
gabbrenn <gabvladimirbrenn@gmail.com>
2023-12-06 18:09:40 +0000

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define Write_file function"""
+"""Define write_file function"""
 
 
 def write_file(filename="", text=""):
-    """Create and Write in file"""
-    with open(filename,'w+',encoding='utf-8') as file:
+    """Create and write in file"""
+    with open(filename, 'w+', encoding='utf-8') as file:
         file.write(text)
     return len(text)
