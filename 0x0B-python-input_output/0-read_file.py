6f8c38cc065df5908e638549307812def8742ac0
gabbrenn <gabvladimirbrenn@gmail.com>
2023-12-06 17:18:50 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Define reaf file"""
+"""Define read file"""
+
 
 def read_file(filename=""):
-	"""use of with and print content"""
-	with open(filename,encoding='utf-8') as file:
-		print(file.read(),end="")
+    """Use 'with' and print content"""
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
+
