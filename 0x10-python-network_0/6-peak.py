c51a7407c9f7b9ae6c1dcd0600da0665554b95f8
gabbrenn <gabvladimirbrenn@gmail.com>
2024-02-29 14:11:03 -0800

    Http Protocol

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Deal with nunbers."""
+
+
+def find_peak(list_of_integers):
+    """Find the largwrst in a poll."""
+    ans = list_of_intgeres[0]
+    for nun in list_of_integers:
+        if num > ans:
+            ans = num
+    return(num)
